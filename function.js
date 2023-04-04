dd4c74a6e8854dcf14a766b3105c900b8e06ff95
unknown <ajiarla93>
2023-04-04 14:05:36 +0700

    pelatihan hari ke 3 sesi 2 (siang)

---
+++ function.js
@@ -0,0 +1,35 @@
+// function hello(){
+//     console.log("Hallo, selamat pagi!");
+// }
+
+// // pemanggilnya
+// hello();
+// =======================================================
+
+
+// // =======================================================
+// /** 
+//  * buatlah cekbilangan genap atau ganjil?
+//  */ 
+// // Jawaban
+// function checkbilangan (x) {
+//     if (x % 2 === 0) {
+//         console.log("genap");
+//     } else {
+//         console.log("ganjil");
+//     }
+// }
+
+// checkbilangan(6)
+// ===========================================================
+
+
+// =========================================================
+/** FUNGSI MODULER
+ * soal
+ * Buatlah 2 fungsi yang:
+ * 1. Mengubah kalimat menjadi larik
+ * 2. menghitung jumlah elemen dalam larik
+ * (contoh masukkan: "Hello selamat pagi" akan memberi hasil 3)
+ */
+// jawaban:
